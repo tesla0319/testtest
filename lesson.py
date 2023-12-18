ceba746cb4b041332ceb253e44fb18a7aa655926
terayama <tomohiro_terayama@totec.co.jp>
2023-12-18 09:44:17 +0900

    1215

--- lesson.py
+++ lesson.py
@@ -1,3 +1,9 @@
-print('##########################')
-print('hello world')
-print('##########################')
+def sumple_func(x,l=[]):
+    l.append(x)
+    return l
+
+y=[1,2,3]
+r=sumple_func(100,y)
+print(r)
+r=sumple_func(200,y)
+print(r)
